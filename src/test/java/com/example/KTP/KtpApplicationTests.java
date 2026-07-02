bf273d5397faea6f894f18297c8d33c2fc02dde6
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-07-02 18:56:30 +0700

    initial project setup with Spring Boot and Maven

---
+++ src/test/java/com/example/KTP/KtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
